69a6db16460242f82feb0c9adccf647184680d24
SamBishop06 <samuelbishop06@yahoo.com>
2024-05-04 14:33:53 -0500

    resolver.js

--- server/config/connection.js
+++ server/config/connection.js
@@ -3,10 +3,10 @@ require('dotenv').config();
 
 mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/googlebooks');
 
-mongoose.connect(uri, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-})
-.then(() => console.log(`Connected to MongoDB: ${mongoose.connection.host}`))
-.catch((err) => console.error('MongoDB connection error:', err));
+// mongoose.connect(uri, {
+//     useNewUrlParser: true,
+//     useUnifiedTopology: true
+// })
+// .then(() => console.log(`Connected to MongoDB: ${mongoose.connection.host}`))
+// .catch((err) => console.error('MongoDB connection error:', err));
 module.exports = mongoose.connection;
